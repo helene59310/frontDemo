f549e4a50e423876e994cd86a81dac9ca67cbe39
LnVsr59 <helene.vasseur1@viacesi.fr>
2026-01-27 11:40:50 +0100

    création composant produits et composant add-produit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  standalone: true,
+  imports: [],
+  templateUrl: './produits.component.html',
+  styleUrl: './produits.component.css'
+})
+export class ProduitsComponent {
+
+  produits : string[];
+
+  constructor() {
+    this.produits = ["PC Asus", "PC Test", "PC Apple"];
+  }
+}
