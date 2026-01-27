c1e527c70fc3488c7f5f02ada0b73e69430aa3ef
LnVsr59 <helene.vasseur1@viacesi.fr>
2026-01-27 13:02:01 +0100

    création modèle Produit et modification du fichier produits.ts ainsi que du fichier produits.html pour afficher un tableau de produits (en dur)

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produits',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 export class ProduitsComponent {
 
-  produits : string[];
+  produits: Produit[];
 
   constructor() {
-    this.produits = ["PC Asus", "PC Test", "PC Apple"];
+    this.produits = [
+      {
+        idProduit: 1, nomProduit: "PC Asus", prixProduit: 3000.600, dateCreation: new Date("01/14/2011")
+      },
+      {
+        idProduit: 2, nomProduit: "Imprimante Epson", prixProduit: 450, dateCreation: new Date("12/17/2010")
+      },
+      {
+        idProduit: 3, nomProduit: "Tablette Samsung", prixProduit: 900.123, dateCreation: new Date("02/20/2020")
+      }
+    ];
   }
 }
