058c91ddf51b68ae1e9fbed7ef8abef3021406de
LnVsr59 <helene.vasseur1@viacesi.fr>
2026-02-03 17:54:01 +0100

    ajout formulaire d'ajout de produit

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-add-produit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-produit.component.html',
   styleUrl: './add-produit.component.css'
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
 
+  newProduit = new Produit();
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  addProduit() {
+    console.log(this.newProduit);
+  }
 }
